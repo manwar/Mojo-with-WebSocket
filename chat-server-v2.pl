2ed1d9c18b120cf3944815d5c230b765a3cdd387
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2026-02-10 23:53:31 +0000

    - Update online users list from all processes.

--- chat-server-v2.pl
+++ chat-server-v2.pl
@@ -14,6 +14,27 @@ my @history = ();
 
 my $process_id = sprintf "%p", \$redis;
 
+# Periodic cleanup: remove stale users every 60 seconds
+Mojo::IOLoop->recurring(60 => sub {
+    # Remove users who haven't been seen in 2 minutes
+    my $cutoff = time - 120;
+    my $db = $redis->db;
+
+    # Get all user session IDs
+    $db->hgetall('chat:users' => sub {
+        my ($db, $err, $result) = @_;
+        return if $err;
+
+        my $user_hash = $result || {};
+        for my $session_id (keys %$user_hash) {
+            my $data = Mojo::JSON::decode_json($user_hash->{$session_id});
+            if ($data->{last_seen} < $cutoff) {
+                $redis->db->hdel('chat:users', $session_id);
+            }
+        }
+    });
+});
+
 $pubsub->json('chat:messages')
        ->listen('chat:messages' => sub {
             my ($pubsub, $data, $channel) = @_;
@@ -41,6 +62,18 @@ websocket '/chat' => sub ($c) {
 
     $clients->{$id} = { tx => $c->tx, name => 'Anonymous' };
 
+    # Heartbeat: update every 30 seconds
+    my $heartbeat = Mojo::IOLoop->recurring(30 => sub {
+        return unless $clients->{$id};  # User disconnected
+
+        my $user_data = Mojo::JSON::encode_json({
+            username  => $clients->{$id}{name},
+            last_seen => time
+        });
+
+        $redis->db->hset('chat:users', $id, $user_data);
+    });
+
     $c->on(json => sub ($self, $data) {
         if ($data->{type} eq 'typing') {
             broadcast({
@@ -51,6 +84,14 @@ websocket '/chat' => sub ($c) {
         }
         elsif ($data->{type} eq 'join') {
             $clients->{$id}{name} = $data->{name};
+
+            # Add user to Redis
+            my $user_data = Mojo::JSON::encode_json({
+                username  => $data->{name},
+                last_seen => time
+            });
+
+            $redis->db->hset('chat:users', $id, $user_data);
 
             for my $old_msg (@history) {
                 $c->send({json => $old_msg});
@@ -79,6 +120,13 @@ websocket '/chat' => sub ($c) {
     $c->on(finish => sub {
         my $name = $clients->{$id}{name};
         delete $clients->{$id};
+
+        # Remove heartbeat timer
+        Mojo::IOLoop->remove($heartbeat);
+
+        # Remove user from Redis
+        $redis->db->hdel('chat:users', $id);
+
         broadcast({ type => 'system', text => "$name left" });
         send_user_list();
     });
@@ -96,8 +144,33 @@ sub broadcast ($msg, $exclude_id = undef) {
 }
 
 sub send_user_list {
-    my @names = sort map { $_->{name} } values %$clients;
-    broadcast({ type => 'users', list => \@names });
+    # Get all active users from Redis (across all processes)
+    $redis->db->hgetall('chat:users' => sub {
+        my ($db, $err, $result) = @_;
+
+        if ($err) {
+            warn "Error fetching users: $err";
+            return;
+        }
+
+        my %seen_names;
+        my $cutoff    = time - 120;
+        my $user_hash = $result || {};
+
+        for my $session_id (keys %$user_hash) {
+            my $data = Mojo::JSON::decode_json($user_hash->{$session_id});
+
+            # Skip stale users
+            next if $data->{last_seen} < $cutoff;
+
+            # Deduplicate by username
+            $seen_names{$data->{username}} = 1;
+        }
+
+        my @names = sort keys %seen_names;
+
+        broadcast({ type => 'users', list => \@names });
+    });
 }
 
 app->start;
